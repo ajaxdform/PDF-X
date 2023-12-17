326f278cfc3659b6698ee5d5994021049b634a7d
Shubham <shubhamsalaskar03@gmail.com>
2023-12-17 19:54:24 +0530

    Stage  Complete

--- testPDF.js
+++ testPDF.js
@@ -13,9 +13,9 @@ const mergePDF= async (p1, p2) => {
 //   await merger.add('pdf2.pdf', '4, 7, 8'); // merge the pages 4, 7 and 8
 //   await merger.add('pdf3.pdf', '3 to 5'); //merge pages 3 to 5 (3,4,5)
 //   await merger.add('pdf3.pdf', '3-5'); //merge pages 3 to 5 (3,4,5)
-
-  await merger.save('public/merged.pdf'); //save under given name and reset the internal document
-  
+  let date = new Date().getTime();
+  await merger.save(`public/${date}.pdf`); //save under given name and reset the internal document
+  return date;
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
   // fs.writeSync('merged.pdf', mergedPdfBuffer);
